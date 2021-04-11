b7d604df969f93c7e21cbdd64ceb2e196846278c
blurname <naughtybao@outlook.com>
2021-04-11 20:07:38 +0800

    menu

---
+++ src/components/menu.js
@@ -0,0 +1,41 @@
+export const menus = [
+  {
+    desc: 'button',
+    data: {
+      type: isButton,
+      value: 'button',
+      style: {
+        top: 0,
+        left: 0,
+        with: 80,
+        height: 30,
+      },
+    },
+  },
+  {
+    desc: 'text',
+    data: {
+      type: isText,
+      value: 'text',
+      style: {
+        top: 0,
+        left: 0,
+        with: 80,
+        height: 30,
+      },
+    },
+  },
+  {
+    desc: 'text',
+    data: {
+      type: isText,
+      value: 'text',
+      style: {
+        top: 0,
+        left: 0,
+        with: 80,
+        height: 30,
+      },
+    },
+  },
+];
